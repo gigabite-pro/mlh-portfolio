1e86e4ce288fb6a61f30cb6032ec8cf738b53fc8
Vaibhav Sharma <rajmachawal19@gmail.com>
2024-07-19 12:34:58 +0530

    updated redeploy

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-tmux kill-server
-tmux new-session -d -s new-deployment
-tmux send-keys -t new-deployment:0 'cd mlh-portfolio' C-m
-tmux send-keys -t new-deployment:0 'git fetch && git reset origin/main --hard' C-m
-tmux send-keys -t new-deployment:0 'source python3-virtualenv/bin/activate' C-m
-tmux send-keys -t new-deployment:0 'pip install -r requirements.txt' C-m
-tmux send-keys -t new-deployment:0 'flask run --host=0.0.0.0' C-m
+systemctl stop myportfolio
+cd /mlh-portfolio
+git fetch && git reset origin/main --hard
+source python3-virtualenv/bin/activate
+pip install -r requirements.txt
+systemctl start myportfolio
