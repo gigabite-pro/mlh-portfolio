01d685d0109027f39e73224e3efb42ae57a3b20e
Vaibhav Sharma <rajmachawal19@gmail.com>
2024-08-01 02:21:31 +0530

    modified redeploy-site.sh

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-systemctl stop myportfolio
 cd /mlh-portfolio
 git fetch && git reset origin/main --hard
-source python3-virtualenv/bin/activate
-pip install -r requirements.txt
-systemctl start myportfolio
+docker compose -f docker-compose.prod.yml down
+docker compose -f docker-compose.prod.yml up -d --build
